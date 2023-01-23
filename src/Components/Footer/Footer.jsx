2ddc48588cf3e9a617d20cfb7a2dbed936d62780
eitikasharma <eitika171249.cse@chitkara.edu.in>
2023-01-24 01:52:07 +0530

    Netflix Clone

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>Questions? Call 1-844-505-2993</h3>
+        <table>
+            <tbody>
+            <tr>
+                <td>FAQ</td>
+                <td>Help Center</td>
+                <td>Account</td>
+                <td>Media Center</td>
+            </tr>
+            <tr>
+                <td>Investor Retails</td>
+                <td>Jobs</td>
+                <td>Redeem Gift Cards</td>
+                <td>Buy Gift Cards</td>
+            </tr>
+            <tr>
+                <td>Ways To Watch</td>
+                <td>Terms Of Use</td>
+                <td>Privacy</td>
+                <td>Cookie Preferences</td>
+            </tr>
+            <tr>
+                <td>Corporate Information</td>
+                <td>Contact Us</td>
+                <td>Speed Test</td>
+                <td>Legal Notices</td>
+            </tr>
+            <tr>
+                <td>Netflix Originls</td>
+            </tr>
+            </tbody>
+        </table>
+    </footer>
+  )
+}
+
+export default Footer
